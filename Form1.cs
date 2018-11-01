6dbc27ecccbd84d5ce7501e788f989779b4fce82
BehniaFarazan <b.farazan@gmail.com>
2018-11-01 11:16:21 +0100

    MergeDone

--- Form1.cs
+++ Form1.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 // This is the code for your desktop app.
 // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-
+//
 namespace FunCalculator
 {
     public partial class Form1 : Form
